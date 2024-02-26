0ed6cb9904bc6b4ca19af5ec76332d14a625702b
Aleksandr <shaban4@yandex.ru>
2024-02-27 02:02:38 +0700

    fix bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,34 @@
 Fancybox.bind("[data-fancybox]", {});
 
 
-(() => {
-    const item = document.querySelector(".nav-networks__link--showreel");
-    window.addEventListener("scroll", (e) => {
-        if (window.pageYOffset > 0) {
-            item.classList.add("nav-networks__link--is-show");
-        } else {
-            item.classList.remove("nav-networks__link--is-show");
-        }
-    });
-})();
+// (() => {
+//     const item = document.querySelector(".nav-networks__link--showreel");
+//     window.addEventListener("scroll", (e) => {
+//         if (window.pageYOffset > 0) {
+//             item.classList.add("nav-networks__link--is-show");
+//         } else {
+//             item.classList.remove("nav-networks__link--is-show");
+//         }
+//     });
+// })();
 
-(() => {
-    const btn = document.querySelector(".js-open-menu");
-    const header = document.querySelector(".header");
-    const nav = document.querySelector(".nav");
+// (() => {
+//     const btn = document.querySelector(".js-open-menu");
+//     const header = document.querySelector(".header");
+//     const nav = document.querySelector(".nav");
 
-    btn.addEventListener("click", () => {
-        if (!header.classList.contains("header--is-active")) {
-            header.classList.add("header--is-active");
-            document.body.classList.add("hidden");
-            nav.style.display = "flex";
-        } else {
-            header.classList.remove("header--is-active");
-            document.body.classList.remove("hidden");
-            setTimeout(() => (nav.style.display = "none"), 300);
-        }
-    });
-})();
+//     btn.addEventListener("click", () => {
+//         if (!header.classList.contains("header--is-active")) {
+//             header.classList.add("header--is-active");
+//             document.body.classList.add("hidden");
+//             nav.style.display = "flex";
+//         } else {
+//             header.classList.remove("header--is-active");
+//             document.body.classList.remove("hidden");
+//             setTimeout(() => (nav.style.display = "none"), 300);
+//         }
+//     });
+// })();
 
 if (document.querySelector('.case-brig-info__slider')) { // Указываем скласс нужного слайдера
     const sliders = document.querySelectorAll('.case-brig-info__slider');
